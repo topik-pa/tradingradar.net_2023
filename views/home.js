36aa9e920b0952f0e0148318474edc78837f0d39
Marco Pavan <marco.pavan@vivabiocell.it>
2023-06-22 20:49:30 +0200

    Uptrends

--- views/home.js
+++ views/home.js
@@ -1,15 +1,18 @@
 import perfMonth from '../components/home/perf-month/perf-month.js'
 import perfYear from '../components/home/perf-year/perf-year.js'
+import upTrends from '../components/home/uptrends/uptrends.js'
 import style from './home.css' assert { type: 'css' };
 import style2 from '../components/home/perf-month/perf-month.css' assert { type: 'css' };
 import style3 from '../components/home/perf-year/perf-year.css' assert { type: 'css' };
+import style4 from '../components/home/uptrends/uptrends.css' assert { type: 'css' };
 
 export const home = {
   init: async () => {
     console.log('Home page')
-    document.adoptedStyleSheets = [style, style2, style3];
+    document.adoptedStyleSheets = [style, style2, style3, style4];
     // shadowRoot.adoptedStyleSheets = [style, style2, style3];
-    perfMonth.init()
+    // perfMonth.init()
     perfYear.init()
+    upTrends.init()
   }
 }
