684d03bd7aa5755df4652a23d63287dfbf185e6f
Marco Pavan <marco.pavan@vivabiocell.it>
2023-06-23 11:27:32 +0200

    Socials in HP

--- views/home.js
+++ views/home.js
@@ -7,15 +7,16 @@ import style2 from '../components/home/perf-month/perf-month.css' assert { type:
 import style3 from '../components/home/perf-year/perf-year.css' assert { type: 'css' };
 import style4 from '../components/home/uptrends/uptrends.css' assert { type: 'css' };
 import style5 from '../components/home/downtrends/downtrends.css' assert { type: 'css' };
+import style6 from '../components/home/socials/socials.css' assert { type: 'css' };
 
 export const home = {
   init: async () => {
     console.log('Home page')
-    document.adoptedStyleSheets = [style, style2, style3, style4, style5];
+    document.adoptedStyleSheets = [style, style2, style3, style4, style5, style6];
     // shadowRoot.adoptedStyleSheets = [style, style2, style3];
-    perfMonth.init()
-    perfYear.init()
-    upTrends.init()
-    downTrends.init()
+    //perfMonth.init()
+    //perfYear.init()
+    //upTrends.init()
+    //downTrends.init()
   }
 }
