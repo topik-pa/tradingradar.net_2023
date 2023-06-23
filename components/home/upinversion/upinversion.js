ee73a065c844330985b6ab01ed2183206c944121
Marco Pavan <marcopavan.mp@gmail.com>
2023-06-23 18:36:43 +0200

    Upinversion + small

---
+++ components/home/upinversion/upinversion.js
@@ -0,0 +1,63 @@
+import proxyFetch from '../../../scripts/proxyFetch.js'
+let $root; let statuses; const url = 'api/custom'
+
+async function callTheApi () {
+  try {
+    const response = await proxyFetch(url)
+    updateUI(statuses[2])
+    printData(response.body.tiup, 'table')
+  } catch (error) {
+    console.error(error)
+    updateUI(statuses[3])
+  }
+}
+
+function updateUI (status) {
+  $root.classList.remove(...statuses)
+  $root.classList.add(status)
+}
+
+function printData (stocks, target) {
+  const $table = $root.querySelector(target)
+  const $tbody = $table.querySelector('tbody')
+  const $detection = $root.querySelector(target + ' ~ p.detection em')
+  for (let i = 0; i < stocks.length; i++) {
+    const stock = stocks[i]
+    if (i === 0) $detection.innerText = new Date(stock.lastPrice.now).toLocaleString()
+    const $tr = document.createElement('tr')
+    const $td1 = document.createElement('td')
+    const $a = document.createElement('a')
+    $a.innerText = stock.name
+    $a.title = stock.name
+    $a.href = `/analisi/${encodeURI(stock.name?.toLowerCase().replace(/ /g, '-'))}?isin=${stock.isin}`
+    $td1.appendChild($a)
+    $tr.appendChild($td1)
+
+    const $td2 = document.createElement('td')
+    $td2.classList.add('green', 'em')
+    $td2.innerText = stock.lastPrice?.value
+    $tr.appendChild($td2)
+
+    const $td3 = document.createElement('td')
+    $td3.innerText = stock.mm20days?.value
+    $tr.appendChild($td3)
+
+    const $td4 = document.createElement('td')
+    $td4.innerText = stock.mm40days?.value
+    $tr.appendChild($td4)
+
+    $tbody.appendChild($tr)
+  }
+}
+
+const upInversion = {
+  init: () => {
+    console.log('upinversion')
+    $root = document.getElementById('upinversion')
+    statuses = ['idle', 'loading', 'success', 'error']
+    updateUI(statuses[1])
+    callTheApi()
+  }
+}
+
+export default upInversion
