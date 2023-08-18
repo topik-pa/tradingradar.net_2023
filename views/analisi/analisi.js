15377d59db69182c36b14a089636ee3a271f0836
Marco Pavan <marcopavan.mp@gmail.com>
2023-08-18 12:03:42 +0200

    Remove follow (temp)

--- views/analisi/analisi.js
+++ views/analisi/analisi.js
@@ -1,9 +1,11 @@
 import stockInfo from '../../components/analisi/stock_info/stock_info.js'
 import stockAlerts from '../../components/analisi/stock_alerts/stock_alerts.js'
+// import follow from '../../components/shared/follow/follow.js'
 
 export const analisi = {
   init: async () => {
     const stockInfoData = await stockInfo.init()
     await stockAlerts.init(stockInfoData)
+    // follow.init()
   }
 }
