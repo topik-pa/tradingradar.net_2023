61fdec1137e86f228c2706d614fed61c263c6a13
Marco Pavan <marcopavan.mp@gmail.com>
2023-09-15 10:24:55 +0200

    Removed slider on main banner

--- components/home/main-banner/main-banner.js
+++ components/home/main-banner/main-banner.js
@@ -1,26 +1,5 @@
 const banner = {
   init: () => {
-    // eslint-disable-next-line no-new, no-undef
-    const mySiema = new Siema({
-      duration: 200,
-      easing: 'ease-out',
-      perPage: 1,
-      startIndex: 0,
-      draggable: true,
-      loop: true
-    })
-    const interval = setInterval(() => {
-      mySiema.next()
-    }, 5000)
-    const $root = document.getElementById('main-banner')
-    $root.querySelector('.next').addEventListener('click', () => {
-      clearInterval(interval)
-      mySiema.next()
-    })
-    $root.querySelector('.prev').addEventListener('click', () => {
-      clearInterval(interval)
-      mySiema.prev()
-    })
   }
 }
 
